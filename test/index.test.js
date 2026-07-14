600beca8d04db2dab19b478a57171ed2c4716619
Teryn Derosier <terynderosier100@icloud.com>
2026-07-14 16:46:51 -0400

    Uncommented tests that past for grading

--- test/index.test.js
+++ test/index.test.js
@@ -3,17 +3,17 @@ const { capitalizeWords, filterActiveUsers, logAction, users } = require('../ind
 
 //Tests that pass
 
-// test(`Capitalizes first letter of each word`, () => {
-//     expect(capitalizeWords("hello world!")).toBe("Hello World!");
-// });
+test(`Capitalizes first letter of each word`, () => {
+    expect(capitalizeWords("hello world!")).toBe("Hello World!");
+});
 
-// test(`Filter active users from inactive users`, () => {
-//     expect(filterActiveUsers(users)).toEqual([{name: "Lisa", isActive: true}, {name: "Sharron", isActive: true}]);
-// });
+test(`Filter active users from inactive users`, () => {
+    expect(filterActiveUsers(users)).toEqual([{name: "Lisa", isActive: true}, {name: "Sharron", isActive: true}]);
+});
 
-// test(`Logs if a user has logged out and adds a timestamp`, () => {
-//     expect(logAction("log out", "Patrick")).toContain("User Patrick performed log out")
-// });
+test(`Logs if a user has logged out and adds a timestamp`, () => {
+    expect(logAction("log out", "Patrick")).toContain("User Patrick performed log out")
+});
 
 //Edge Cases
 
